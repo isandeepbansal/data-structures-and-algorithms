299aa89026d264fbccc7402f0ca41bf2ebc5a92d
isandeepbansal <64344157+isandeepbansal@users.noreply.github.com>
2021-06-13 23:49:19 +0530

    bubble sort test added

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
+🧑‍💻 Computer science is the science of making trade offs.
+
 # Data Structure and Algorithms
 
 **Data structure** is a particular way of organizing data in a computer so that it can be used effectively. Example: Arrays, Hash Tables, Linked List, Stack, Queue and etc. **Algorithm** is a sequence of steps to solve a problem.
 
-## Big O
+# Big O
 
 Big O is a way of talking about how efficient an algorithm is. Big O is about broad stroke.
 
 ![BigO](https://github.com/isandeepbansal/ds-and-algorithms/blob/main/assets/big-o.png)
+
+# Bubble Sort
+
+- Bubble sort guarantee that equivalent items come back in the order they were in.
+- Bubble sort is destructive it means it change the original array.
--- bubble-sort/bubble-sort.test.js
+++ bubble-sort/bubble-sort.test.js
@@ -1,22 +1,30 @@
-function bubbleSort(nums) {
+// Bubble sort
+Array.prototype.bubbleSort = function () {
   let swapped = false;
   do {
     swapped = false;
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] > nums[i + 1]) {
-        const temp = nums[i];
-        nums[i] = nums[i + 1];
-        nums[i + 1] = temp;
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] > this[i + 1]) {
+        let temp = this[i];
+        this[i] = this[i + 1];
+        this[i + 1] = temp;
         swapped = true;
       }
     }
   } while (swapped);
 
-  return nums;
-}
+  return this;
+};
 
-test.skip("bubble sort test 1", function () {
-  const nums = [8, 4, 6, 2, 9];
-  const sortedNums = bubbleSort(nums);
-  expect(sortedNums).toEqual([2, 4, 6, 8, 9]);
+// Tests
+test("bubble sort test 1", function () {
+  const nums = [5, 3, 1, 2, 8, 6];
+  const sortedNums = nums.bubbleSort();
+  expect(sortedNums).toEqual([1, 2, 3, 5, 6, 8]);
+});
+
+test("bubble sort test 2", function () {
+  const nums = [2, 2, 1, 8, 2, 1, 3];
+  const sortedNums = nums.bubbleSort();
+  expect(sortedNums).toEqual([1, 1, 2, 2, 2, 3, 8]);
 });
